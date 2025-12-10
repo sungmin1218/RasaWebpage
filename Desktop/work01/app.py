d4c27306a3e611a81d4328ac87b72359828494d8
charmingsoohan <suhan2200@naver.com>
2025-12-10 18:00:57 +0900

    first work

---
+++ Desktop/work01/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/notice")
+def notice():
+    return render_template("notice.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
